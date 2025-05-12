e56116a103c229dcf95b9a189d6144a0d87f5483
llcky8 <brovarskoy8@gmail.com>
2025-05-12 05:14:36 +0400

    credited lab

---
+++ 6 semester/lab1/lab1/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace lab1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            processButton = new Button();
+            inputTextBox = new TextBox();
+            label = new Label();
+            SuspendLayout();
+            // 
+            // processButton
+            // 
+            processButton.Anchor = AnchorStyles.None;
+            processButton.Font = new Font("Microsoft Sans Serif", 14F);
+            processButton.Location = new Point(178, 259);
+            processButton.Name = "processButton";
+            processButton.Size = new Size(236, 54);
+            processButton.TabIndex = 0;
+            processButton.Text = "Посчитать";
+            processButton.UseVisualStyleBackColor = true;
+            processButton.Click += ProcessButton_Click;
+            // 
+            // inputTextBox
+            // 
+            inputTextBox.Font = new Font("Microsoft Sans Serif", 14F);
+            inputTextBox.Location = new Point(178, 164);
+            inputTextBox.Name = "inputTextBox";
+            inputTextBox.Size = new Size(236, 34);
+            inputTextBox.TabIndex = 1;
+            inputTextBox.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label
+            // 
+            label.AutoSize = true;
+            label.Font = new Font("Microsoft Sans Serif", 14F);
+            label.Location = new Point(91, 68);
+            label.Name = "label";
+            label.Size = new Size(446, 29);
+            label.TabIndex = 2;
+            label.Text = "Введите список чисел через пробел:";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(600, 390);
+            Controls.Add(label);
+            Controls.Add(inputTextBox);
+            Controls.Add(processButton);
+            Name = "MainForm";
+            Text = "Счетчик повторяющихся элементов";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button processButton;
+        private TextBox inputTextBox;
+        private Label label;
+    }
+}
---
+++ 6 semester/lab1/lab1/MainForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            this.BackColor = System.Drawing.Color.LightBlue;
+            InitializeComponent();
+        }
+        private void ProcessButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int[] numbers = inputTextBox.Text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                               .Select(int.Parse)
+                                               .ToArray();
+                int result = CountEqualPrevious(numbers);
+
+                ResultForm resultForm = new ResultForm(numbers, result);
+                resultForm.ShowDialog(this); // Открывает окно поверх основного
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Введите корректные числа через пробел", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private int CountEqualPrevious(int[] numbers)
+        {
+            if (numbers.Length < 2) return 0;
+            int count = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                    count++;
+            }
+            return count;
+        }
+    }
+    public class ResultForm : Form
+    {
+        public ResultForm(int[] numbers, int result)
+        {
+            this.Text = "Результат";
+            this.Size = new System.Drawing.Size(380, 125);
+            this.StartPosition = FormStartPosition.CenterParent;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            // Создаем текст с введенными числами
+            string numbersText = "Введенные числа: " + string.Join(", ", numbers);
+
+            // Создаем метку для отображения введенных чисел
+            Label numbersLabel = new Label()
+            {
+                Text = numbersText,
+                AutoSize = true,
+                Location = new System.Drawing.Point(10, 20)
+            };
+
+            // Создаем метку для отображения результата
+            Label resultLabel = new Label()
+            {
+                Text = "Количество элементов, равных своему предыдущему: " + result,
+                AutoSize = true,
+                Location = new System.Drawing.Point(10, 50)
+            };
+
+            // Добавляем метки на форму
+            this.Controls.Add(numbersLabel);
+            this.Controls.Add(resultLabel);
+        }
+    }
+}
